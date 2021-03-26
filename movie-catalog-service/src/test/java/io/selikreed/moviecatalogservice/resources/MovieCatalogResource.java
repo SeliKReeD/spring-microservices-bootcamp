5ddce5103b5b7c100c6adf60bbc89f68f741656b
Vladyslav Seliverstov <seliverstovvladislavpb@gmail.com>
2021-03-26 16:58:46 +0100

    init commit

---
+++ movie-catalog-service/src/test/java/io/selikreed/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,10 @@
+package io.selikreed.moviecatalogservice.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResource {
+
+    
+    
+}
