12c5060164a991b99d88b2600414e6dc1682b01d
Vladyslav Seliverstov <seliverstovvladislavpb@gmail.com>
2021-03-26 18:50:56 +0100

    rest template usage for calling other microservice

--- movie-catalog-service/src/main/java/io/selikreed/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/selikreed/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,22 +1,39 @@
 package io.selikreed.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.selikreed.model.CatalogItem;
+import io.selikreed.model.Movie;
+import io.selikreed.model.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
     
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable(name = "userId") String userId) {
-        return Collections.singletonList(
-            new CatalogItem("Transformers", "Test", 4)
+
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 5),
+            new Rating("6789", 1)
         );
+
+        return ratings.stream().map(
+            rating ->  {
+                Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                return new CatalogItem(movie.getName(), "Test", rating.getRating());
+            }
+        ).collect(Collectors.toList());
     }
 }
